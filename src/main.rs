aa1a5c4bcca14f27966bcd1839ebf119e0006621
David Begin <davidmichaelbe@gmail.com>
2015-05-15 23:24:26 -0700

    man that is pretty magical and still confusing

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,31 @@
 #![allow(dead_code)]
 
 use std::thread;
+use std::sync::{Mutex, Arc};
 
 fn main() {
     println!("\nDining Philosophers\n");
 
-    let p1 = Philosopher::new("Baruch Spinoza");
-    let p2 = Philosopher::new("Gilles Deleuze");
-    let p3 = Philosopher::new("Karl Marx");
-    let p4 = Philosopher::new("Friedrich Nietzsche");
-    let p5 = Philosopher::new("Michel Foucault");
+    let table = Arc::new(Table { forks: vec![
+        Mutex::new(()),
+        Mutex::new(()),
+        Mutex::new(()),
+        Mutex::new(()),
+        Mutex::new(()),
+    ]});
+
+    let p1 = Philosopher::new("Baruch Spinoza", 0, 1);
+    let p2 = Philosopher::new("Gilles Deleuze", 1, 2);
+    let p3 = Philosopher::new("Karl Marx", 2, 3);
+    let p4 = Philosopher::new("Friedrich Nietzsche", 3, 4);
+    let p5 = Philosopher::new("Michel Foucault", 0, 4);
     let philosophers = vec![p1, p2, p3, p4, p5];
 
     let handles: Vec<_> = philosophers.into_iter().map(|p| {
+        let table = table.clone();
+
         thread::spawn(move || {
-            p.eat();
+            p.eat(&table);
         })
     }).collect();
 
@@ -24,18 +35,29 @@ fn main() {
     }
 }
 
+struct Table {
+    forks: Vec<Mutex<()>>
+}
+
 struct Philosopher {
-    name: String
+    name: String,
+    left: usize,
+    right: usize
 }
 
 impl Philosopher {
-    fn new(name: &str) -> Philosopher {
+    fn new(name: &str, left: usize, right: usize) -> Philosopher {
         Philosopher {
             name: name.to_string(),
+            left: left,
+            right: right,
         }
     }
 
-    fn eat(&self) {
+    fn eat(&self, table: &Table) {
+        let _left = table.forks[self.left].lock().unwrap();
+        let _right = table.forks[self.right].lock().unwrap();
+
         println!("{} is eating...", self.name);
         thread::sleep_ms(100);
         println!("{} is finished eating.", self.name);
