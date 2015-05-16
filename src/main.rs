bd0d4a9d54b06bc83647bebe91666d54963620ac
David Begin <davidmichaelbe@gmail.com>
2015-05-15 22:55:14 -0700

    thread:sleep_ms
    a good thing to learn in any language for great pranks

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+#![allow(unused_variables)]
+#![allow(dead_code)]
+
+use std::thread;
+
 fn main() {
     println!("\nDining Philosophers\n");
+
+    let p1 = Philosopher::new("Baruch Spinoza");
+    let p2 = Philosopher::new("Gilles Deleuze");
+    let p3 = Philosopher::new("Karl Marx");
+    let p4 = Philosopher::new("Friedrich Nietzsche");
+    let p5 = Philosopher::new("Michel Foucault");
+    let philosophers = vec![p1, p2, p3, p4, p5];
+
+    for philo in &philosophers {
+        philo.eat();
+    }
 }
+
+struct Philosopher {
+    name: String
+}
+
+impl Philosopher {
+    fn new(name: &str) -> Philosopher {
+        Philosopher {
+            name: name.to_string(),
+        }
+    }
+
+    fn eat(&self) {
+        println!("{} is eating...", self.name);
+        thread::sleep_ms(100);
+        println!("{} is finished eating.", self.name);
+    }
+}
+
+
